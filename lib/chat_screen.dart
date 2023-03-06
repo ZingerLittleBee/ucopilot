3f9cc7b6a670a5aa328822a89a201a71e602017c
ZingerLittleBee <6970999@gmail.com>
2023-03-07 03:12:19 +0800

    feat: 🎸 scroll bottom

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,6 +1,6 @@
-import 'package:bubble/bubble.dart';
 import 'package:dart_openai/openai.dart';
 import 'package:flutter/material.dart';
+import 'package:markdown_widget/widget/markdown.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -12,8 +12,32 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = TextEditingController();
   final List<ChatMessage> _messages = <ChatMessage>[];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   void _handleSubmitted(String text) async {
+    if (text.isEmpty) return;
+
     _textController.clear();
     ChatMessage message = ChatMessage(
       text: text,
@@ -30,8 +54,6 @@ class ChatScreenState extends State<ChatScreen> {
         OpenAIChatCompletionChoiceMessageModel(content: text, role: "user"),
       ],
     );
-
-    debugPrint('chatCompletion.choices: ${chatCompletion.choices}');
 
     for (var element in chatCompletion.choices) {
       ChatMessage message = ChatMessage(
@@ -80,17 +102,41 @@ class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text('Chat'),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 50.0),
+        child: FloatingActionButton(
+          onPressed: () {
+            scrollController.animateTo(
+              scrollController.position.maxScrollExtent,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeOut,
+            );
+          },
+          child: const Icon(Icons.arrow_downward),
+        ),
+      ),
       body: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                children: <Widget>[
+                  ListView.separated(
+                      padding: const EdgeInsets.all(8.0),
+                      physics: const NeverScrollableScrollPhysics(),
+                      reverse: true,
+                      shrinkWrap: true,
+                      itemBuilder: (_, int index) => _messages[index],
+                      itemCount: _messages.length,
+                      separatorBuilder: (BuildContext context, int index) =>
+                          const SizedBox(height: 8.0))
+                ],
+              ),
             ),
           ),
           const Divider(
@@ -123,52 +169,57 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const nickNameStyle = TextStyle(
-        color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 12);
+        color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 12);
     final _messageType = messageType ?? MessageType.info;
 
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: type == RoleType.user
-          ? ListTile(
-              trailing: const CircleAvatar(child: Text('User')),
-              title: const Text(
-                'User',
-                textAlign: TextAlign.end,
-                style: nickNameStyle,
-              ),
-              subtitle: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Bubble(
-                    nip: BubbleNip.rightTop,
-                    color: Colors.blue[100],
-                    padding: const BubbleEdges.all(10),
-                    child: Text(
-                      text,
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
+    return type == RoleType.user
+        ? ListTile(
+            title: const Text(
+              'User',
+              textAlign: TextAlign.end,
+              style: nickNameStyle,
+            ),
+            subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.blue.shade100,
+                  ),
+                  child: Text(
+                    text,
+                    textAlign: TextAlign.end,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
+                ),
+              ],
+            ),
+            contentPadding: EdgeInsets.zero)
+        : ListTile(
+            title: const Text(
+              'Bot',
+              textAlign: TextAlign.start,
+              style: nickNameStyle,
+            ),
+            subtitle: Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: _messageType == MessageType.info
+                    ? Colors.white
+                    : Colors.red[200],
               ),
-            )
-          : ListTile(
-              leading: const CircleAvatar(child: Text('Bot')),
-              title: const Text('Bot', style: nickNameStyle),
-              subtitle: Row(
-                children: [
-                  Bubble(
-                    nip: BubbleNip.leftTop,
-                    padding: const BubbleEdges.all(10),
-                    child: Text(
-                      text,
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                    ),
-                  ),
-                ],
+              child: MarkdownWidget(
+                data: text,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                selectable: true,
               ),
             ),
-    );
+            contentPadding: EdgeInsets.zero);
   }
 }
